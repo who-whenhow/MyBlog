234f9a92f883a389607a571c8c4c99a25d420f3a
who-whenhow <1835870939@qq,com>
2026-06-01 18:24:21 +0800

    解决网页显示问题

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://whowhenhow123456.eu.org",
-	base: "/",
+	base: "/MyBlog/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
