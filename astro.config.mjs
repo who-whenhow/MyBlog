0deb52188a93e7c7e91344ca8b015063d43896f1
who-whenhow <1835870939@qq,com>
2026-06-01 18:29:39 +0800

    解决网页显示再尝试

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 // https://astro.build/config
 export default defineConfig({
 	site: "https://whowhenhow123456.eu.org",
-	base: "/MyBlog/",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
